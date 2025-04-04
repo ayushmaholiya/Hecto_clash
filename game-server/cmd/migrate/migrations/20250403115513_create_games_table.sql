a42616327011a1c7843458168ae55bb3645ba7b1
eclairjit <ajblazejit95@gmail.com>
2025-04-04 23:40:29 +0530

    refactor games and players table schemas for consistency and correctness

--- game-server/cmd/migrate/migrations/20250403115513_create_games_table.sql
+++ game-server/cmd/migrate/migrations/20250403115513_create_games_table.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS games (
     id BIGSERIAL PRIMARY KEY,
-    hectoc_puzzle CHAR(6) NOT NULL CHECK (hectoc_puzzle ~ '^[0-9]{6}$'),
+    hectoc_puzzle CHAR(6) NOT NULL CHECK (hectoc_puzzle ~ '^[1-9]{6}$'),
     winner_id BIGINT,
-    winning_submission TEXT,
+    winning_submission TEXT [],
     correct_solution TEXT,
-    game_state VARCHAR(11) NOT NULL CHECK (game_state IN ('waiting', 'in_progress', 'completed')),
+    game_state VARCHAR(11) NOT NULL CHECK (game_state IN ('waiting', 'in_progress', 'completed')) DEFAULT 'waiting',
     created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT now()
 );
 -- +goose StatementBegin
